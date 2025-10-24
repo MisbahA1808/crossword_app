c70284b3bceb14afaaf41bf050e26491beeb7705
MisbahA18 <misbahahmad2007@gmail.com>
2025-10-24 19:34:47 +0100

    Add CrosswordManager class with constructor and attributes and add an AddWord() method

---
+++ CrosswordManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrosswordApp
+{
+    internal class CrosswordManager
+    {
+        private Crossword _currentCrossword;
+        public CrosswordManager(Crossword crossword)
+        {
+            _currentCrossword = crossword;
+        }
+
+        public void AddWord(string word, string direction) 
+        { 
+        
+        
+        
+        }
+    }
+}
