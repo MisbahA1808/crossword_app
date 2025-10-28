46de25e126e60c362b3d230364ec75f874c67bb6
MisbahA18 <misbahahmad2007@gmail.com>
2025-10-28 09:04:28 +0000

    Add Activate and Deactivate methods to MenuItem.cs

--- MenuItem.cs
+++ MenuItem.cs
@@ -6,16 +6,34 @@ using System.Threading.Tasks;
 
 namespace CrosswordApp
 {
+    //class that represents each option in the menu (menu item)
     internal class MenuItem
     {
+        //name attribute
         private string _name;
+        //if the men item is active (is it the one being selected at the moment)
         private bool _active;
 
+        //constructor
         public MenuItem(string name)
         {
             _name = name;
             _active = false;
         }
 
+        public string Name { get => _name; set => _name = value; }
+        public bool Active { get => _active; set => _active = value; }
+
+        //method to change active to true
+        public void Activate() 
+        {
+            _active = true;
+        }
+
+        //method to chnage active to false
+        public void Deactivate() 
+        {
+            _active = false;
+        }
     }
 }
