ed3aceb6a81a6de011fa5cd433f99776e3b9563e
MisbahA18 <misbahahmad2007@gmail.com>
2025-10-28 09:08:00 +0000

    Add Menu.cs, initial attributes and constructor

--- Menu.cs
+++ Menu.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace CrosswordApp
 {
+    //class that represents any given menu
     internal class Menu
     {
-        
+        private List<MenuItem> _items;
+        private string _name;
+        private bool _active;
+        private int _activeItemPointer;
 
+        public string Name { get => _name; set => _name = value; }
+        public bool Active { get => _active; set => _active = value; }
 
+        public Menu(string name)
+        {
+            _name = name;
+            _active = false;
+            _items = new List<MenuItem>();
+            _activeItemPointer = 0;
+        }
     }
 }
